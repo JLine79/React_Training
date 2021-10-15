7daaab451c800465bd1d90329cc543ce4a915fc8
Jennifer Line <51475586+JLine79@users.noreply.github.com>
2021-10-15 14:40:16 +0100

    Learning about lifecycle components

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,27 +2,19 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends React.Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-
-  handleDelete = (counterId) => {
-    console.log("Event Handler Called", counterId);
-  };
-
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
     return (
       <div>
-        {this.state.counters.map((counter) => (
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        {counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            value={counter.value}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            counter={counter}
           />
         ))}
       </div>
