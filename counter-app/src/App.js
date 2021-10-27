45266423d858b88c7d66d3b023ad5120b929b31f
Jennifer Line <51475586+JLine79@users.noreply.github.com>
2021-10-27 11:15:00 +0100

    completed tutorial, see Notion notes!

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -13,6 +13,15 @@ class App extends React.Component {
     ],
   };
 
+  constructor(props) {
+    super(props);
+    console.log('App - Constructor');
+  }
+
+  componentDidMount() {
+    console.log('App - Mounted');
+  }
+
   handleIncrement = (counter) => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
@@ -35,6 +44,7 @@ class App extends React.Component {
   };
 
   render() {
+    console.log('App - Rendered');
     return (
       <React.Fragment>
       <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
